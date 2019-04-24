78e7120dec9b304c316dacf890ca0106de6d78ab
Tuan Mai <perhuman@gmail.com>
2019-04-24 15:23:26 +0700

    chore/add method to set up a event handler

--- lib/kafka_handle_event/event_proxy.rb
+++ lib/kafka_handle_event/event_proxy.rb
@@ -1,14 +1,23 @@
 class EventProxy
-  attr_accessor :model_name, :topics, :attribute_mapper
+  attr_accessor :model_name, :topics, :attribute_mapper, :primaries
 
   def initialize(mobel_name)
     @model_name = mobel_name
     @topics = []
-    attribute_mapper = []
+    @attribute_mapper = []
   end
 
   def topic(topic)
     @topics.push(topic)
     @topics = @topics.uniq
+  end
+
+  def primary_column(internal_id, external_id = nil)
+    external_id ||= internal_id
+    @primaries = [internal_id, external_id]
+  end
+
+  def map_column(internal, external, default_value = nil)
+    @attribute_mapper << [internal, external, default_value]
   end
 end
--- spec/event_proxy_spec.rb
+++ spec/event_proxy_spec.rb
@@ -2,12 +2,47 @@ require 'spec_helper'
 require 'kafka_handle_event/event_proxy'
 
 describe EventProxy do
+  let(:model_name) { :model_name }
+  let(:subject) { described_class.new(model_name) }
+
   describe 'model_name' do
-    let(:model_name) { :model_name }
-    let(:subject) { described_class.new(model_name) }
-    
     it 'stores model name' do
       expect(subject.model_name).to equal(model_name)
+    end
+  end
+
+  describe '#topic' do
+    let(:topic_name) { 'topic_name' }
+
+    it 'adds topic to topics list' do
+      subject.topic topic_name
+      expect(subject.topics).to include(topic_name)
+    end
+  end
+
+  describe '#primary_column' do
+    let(:internal_primary_column) { 'id' }
+    let(:external_primary_column) { 'uuid' }
+
+    it 'stores primary column as array' do
+      subject.primary_column internal_primary_column, external_primary_column
+      expect(subject.primaries).to eq([internal_primary_column, external_primary_column])
+    end
+
+    context 'does not pass external_primary_column' do
+      it 'store external primary column as internal primary column' do
+        subject.primary_column internal_primary_column
+        expect(subject.primaries).to eq([internal_primary_column, internal_primary_column])
+      end
+    end
+  end
+
+  describe '#map_column' do
+    let(:default_value) { 'default_value' }
+
+    it 'adds column to attribute_mapper' do
+      subject.map_column :internal, :external, default_value
+      expect(subject.attribute_mapper).to include([:internal, :external, default_value])
     end
   end
 end
